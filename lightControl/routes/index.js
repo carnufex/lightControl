fa584a24d1b4b5534ba24bf241f781ab25b48210
Christopher Rosenvall <Christopher Rosenvall>
2016-11-03 02:28:27 +0100

    massa fippel med bootstrap, hbs och hur saker funkar overall

--- lightControl/routes/index.js
+++ lightControl/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', projectName: 'Light Control'  });
+});
+
+/* GET home page. */
+router.get('/bootstrap', function(req, res, next) {
+  res.render('bootstrap', { title: 'Bootstrap'});
 });
 
 module.exports = router;
